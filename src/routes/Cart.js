3d8df4ee46566a8478908485a1e517448343fd81
daraphista <daraphistadaone@gmail.com>
2022-03-25 16:57:04 +0800

    feat(routes) add Cart route

---
+++ src/routes/Cart.js
@@ -0,0 +1,10 @@
+const Cart = () => {
+  return (
+    <main className="flex-1 flex justify-center py-10" data-testid="cart">
+      <h1 className="text-5xl font-bold">Your Shopping Cart</h1>
+      
+    </main>
+  )
+}
+
+export default Cart
