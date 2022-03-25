51b5d1981c7e5382beea8d23d74477fb9df3eda0
daraphista <daraphistadaone@gmail.com>
2022-03-25 16:56:23 +0800

    feat(routes): create Home route

---
+++ src/routes/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main data-testid="home" className="flex-1 flex justify-center items-center">
+      <h1 className="text-5xl font-bold">This Home Page</h1>
+    </main>
+  )
+}
+
+export default Home;
