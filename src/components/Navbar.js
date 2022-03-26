81c2f5666054814ee8591bfd22083946bc9efa54
daraphista <daraphistadaone@gmail.com>
2022-03-26 21:37:51 +0800

    feat(Navbar): add comments for readability in Navbar.js file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ const Navbar = () => {
     <header role="navigation" className="flex justify-evenly items-center bg-black text-pink-500 p-9">
       <h1 className="text-5xl font-bold">Amazong</h1>
       <ul className="flex w-[50%] justify-around text-3xl cursor-pointer">
+        {/* Create links pointing to corresponding pages/routes */}
         <li>
           <Link to="/">Home</Link>
         </li>
