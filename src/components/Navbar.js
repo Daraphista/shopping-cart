ca083d5a98bf15ee9cbd5de6ffb8ed0b54dfb91d
daraphista <daraphistadaone@gmail.com>
2022-03-27 15:01:28 +0800

    feat(Navbar): Change store name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header role="navigation" className="flex justify-evenly items-center bg-black text-pink-500 p-9">
-      <h1 className="text-5xl font-bold">Amazong</h1>
+      <h1 className="text-5xl font-bold">Fashion Store</h1>
       <ul className="flex w-[50%] justify-around text-3xl cursor-pointer">
         {/* Create links pointing to corresponding pages/routes */}
         <li>
