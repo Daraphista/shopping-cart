3a216db5397af17aff64a2ba1e1dd23be87222d7
daraphista <daraphistadaone@gmail.com>
2022-03-29 11:58:48 +0800

    fix(Cart): lift state to App parent component

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,64 +1,65 @@
-import { useState } from "react";
+import { render } from "@testing-library/react";
+import { useEffect, useState } from "react";
 import uniqid from 'uniqid';
 import CartItemCard from "../components/CartItemCard";
 
 const Cart = (props) => {
-
-  // state to store initial items props
   const [items, setItems] = useState(props.items);
 
-  // function to handle change
-  const handleChange = (itemObject) => {
-    const index = items.indexOf(itemObject);
+  // function to lift change to Cart (current component) state
+  const liftChange = (itemObject, value) => {
+    const index = items.indexOf(itemObject); 
 
-    // create shallow copy of items array
-    const itemsCopy = items;
-    // replace itemObject with the mutated one in itemsCopy
-    itemsCopy[index] = itemObject;
+    const itemObjectCopy = { ...itemObject };
+    itemObjectCopy.quantity = value;
+
+    const itemsCopy = [ ...items ];
+    itemsCopy[index] = itemObjectCopy;
 
     setItems(itemsCopy);
-    setTotalPrice(calculateTotalPrice())
+    props.liftChange(itemsCopy); // lifts change to App (parent component) state so that change is still reflected after rerender
   }
 
   // function to calculate totalPrice
   const calculateTotalPrice = () => {
-    // declare variable to store sum
     let sum = 0;
-
-    // iterate through array with forEach
+    
     items.forEach(item => {
-      // destructure price and quantity properties
       const { price, quantity } = item;
-      // multiply price by quantity and store in variable
+      // multiply item price by quantity
       const totalItemPrice = price * quantity;
-      // add totalItemPrice to sum
       sum += totalItemPrice;
     })
-
-    // limit decimal places of sum and return it
+    
+    // limit decimal numbers to 2
     return sum.toFixed(2);
   }
-
-  // state to store total price
+  
   const [totalPrice, setTotalPrice] = useState(calculateTotalPrice());
 
+  useEffect(() => {
+    setTotalPrice(calculateTotalPrice());
+  })
+
   return (
-    <main className="flex-1 flex flex-col items-center py-10 gap-6" data-testid="cart">
+    <main 
+      className="flex-1 flex flex-col items-center py-10 gap-6"
+      data-testid="cart"
+    >
       <h1 className="text-3xl font-bold">Your Shopping Cart</h1>
       <div className="grid gap-6 w-[50%]">
         {items.map(item => {
-          const key = uniqid();
           return (
             <CartItemCard 
-              product={item} 
-              handleChange={handleChange}
-              key={key}
+              item={item} 
+              liftChange={liftChange} 
             />
           )
         })}
       </div>
       <button className="bg-black text-pink-500 text-3xl rounded-lg
-        p-6 w-[50%]">
+        p-6 w-[50%]"
+      >
         {`Checkout $${totalPrice}`}
       </button>
     </main>
