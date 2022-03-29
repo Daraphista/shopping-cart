af30e9429e945af916fda2af51a2f82979c773ea
daraphista <daraphistadaone@gmail.com>
2022-03-29 12:00:25 +0800

    feat(Home): improved Home ui

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
   return (
-    <main data-testid="home" className="flex-1 flex justify-center items-center">
-      <h1 className="text-5xl font-bold">This Home Page</h1>
+    <main 
+      data-testid="home" 
+      className="flex-1 flex justify-center items-center bg-home-background"
+    >
+      <div className="p-6 bg-white rounded-xl">
+        <h1 className="text-5xl font-bold">
+          Welcome to the <span className="text-pink-500">Fashion Store</span>!
+        </h1>
+        <p className="text-2xl">The easiest and most effective place to max out your boyfriend's credit card!</p>
+      </div>
     </main>
   )
 }
