3a2387448f98cf1fac7982a14e47bac0895535ed
daraphista <daraphistadaone@gmail.com>
2022-03-29 11:55:45 +0800

    fix(CartItemCard): fix bug that rerenders component per input change

--- src/components/CartItemCard.js
+++ src/components/CartItemCard.js
@@ -2,57 +2,70 @@ import { useEffect, useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 
 const CartItemCard = (props) => {
-  // destructure props
-  const { product, handleChange } = props;
+  const { item, liftChange } = props;
+  const { quantity } = item;
 
-  // state to store initial quantity from quantity property from product prop
-  const [quantity, setQuantity] = useState(product.quantity);
-
-  useEffect(() => {
-    // add quantity property to object
-    product.quantity = quantity;
-    handleChange(product);
-  })
-
-  // function to increment quantity
-  const incrementQuantity = () => {
-    setQuantity(quantity + 1);
+  // function to handle input change
+  const handleChange = (e) => {
+    // function to turn input value into a number
+    const inputValue = Number(e.target.value);
+    // lift change to cart (parent component) state
+    liftChange(item, inputValue);
   }
-  // function to decrement quantity
+
+  // function to decrement the quantity of the item object
   const decrementQuantity = () => {
-    setQuantity(quantity - 1);
+    // decrement quantity variable
+    const decrementedQuantity = quantity - 1;
+    // lift change to cart (parent component) state
+    liftChange(item, decrementedQuantity);
+  }
+
+  // function to increment the quantity of the item object
+  const incrementQuantity = () => {
+    // increment quantity variable
+    const incrementedQuantity = quantity + 1;
+    // lift change to cart (parent component) state
+    liftChange(item, incrementedQuantity);
   }
 
   return (
-    <div className="grid grid-rows-[4fr,_2fr] p-2 min-h-[100%] shadow-lg bg-white rounded-md">
+    <div className="grid grid-rows-[4fr,_2fr] p-2 min-h-[100%] 
+      shadow-lg bg-white rounded-md"
+    >
       <div className="flex justify-center">
-        <img className="w-[15vmin] object-contain" src={product.image} alt="lmao"></img>
+        <img 
+          className="w-[15vmin] object-contain"
+          src={item.image}
+          alt="lmao"
+        />
       </div>
       <div className="grid grid-rows-3 items-center">
-        <h1 className="font-bold text-center">{product.title}</h1>
-        <p className="text-2xl text-right">{`$${product.price}`}</p>
-        <form className="flex justify-center gap-2 h-[4vmin]">
+        <h1 className="font-bold text-center">{item.title}</h1>
+        <p className="text-2xl text-right">{`$${item.price}`}</p>
+        <form 
+          className="flex justify-center gap-2 h-[4vmin]"
+          onSubmit={e => {e.preventDefault()}} // prevent form submitting, causing a refresh.
+        >
           <button 
-            className="
-            bg-black text-pink-500 w-[4vmin] rounded-sm flex justify-center 
-            items-center"
-            type="button"
+            className="bg-black text-pink-500 w-[4vmin] rounded-sm
+            flex justify-center items-center disabled:bg-neutral-800"
+            type="button" 
             onClick={decrementQuantity}
-            disabled={quantity<1}
+            disabled={quantity < 1}
           >
             <AiOutlineMinus />
           </button>
           <input 
-            className="w-[20%] text-center text-2xl border-gray-500 
-            border-[1px] rounded-sm" 
-            value={quantity}
-            onChange={handleChange}
-          />
+            className="w-[20%] text-center text-2xl borger-gray-500
+            border-[1px] rounded-sm"
+            value={quantity} 
+            onChange={handleChange} 
+          />      
           <button 
-            className="
-            bg-black text-pink-500 w-[4vmin] rounded-sm flex justify-center 
-            items-center"
-            type="button"
+            className="bg-black text-pink-500 w-[4vmin] rounded-sm
+            flex justify-center items-center"
+            type="button" 
             onClick={incrementQuantity}
           >
             <AiOutlinePlus />
